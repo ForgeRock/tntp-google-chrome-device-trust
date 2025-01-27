50e29cbf8d3fa52d9745601ef07cd582ef6facf7
Matthew Teets <matthewteets@pingidentity.com>
2025-01-27 16:15:47 -0700

    Updated challengeresponse handling and outcomes

--- src/main/java/org/forgerock/am/marketplace/googlechromedevicetrust/GoogleChromeDeviceTrustClient.java
+++ src/main/java/org/forgerock/am/marketplace/googlechromedevicetrust/GoogleChromeDeviceTrustClient.java
@@ -139,7 +139,11 @@ public class GoogleChromeDeviceTrustClient {
 
         // Format and generate the private key
         try {
-            final byte[] privateData = Base64.getDecoder().decode(privateKeyString.replaceAll("-----BEGIN PRIVATE KEY-----", "").replaceAll("-----END PRIVATE KEY-----", "").replaceAll("\\s", ""));
+            final byte[] privateData = Base64.getDecoder().decode(privateKeyString
+                    .replace("-----BEGIN PRIVATE KEY-----", "")
+                    .replace("-----END PRIVATE KEY-----", "")
+                    .replace("\n", "")
+                    .replaceAll("\\s", ""));
             privateKey = KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(privateData));
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
             logger.error(String.valueOf(e));
